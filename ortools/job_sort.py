533a19248a4aba0b960b5e341ab6c61fb2adb405
hsingwenhsu <b05502012@ntu.edu.tw>
2021-01-17 20:29:21 +0800

    dependency example

--- ortools/job_sort.py
+++ ortools/job_sort.py
@@ -4,23 +4,91 @@ import math
 # Import Python wrapper for or-tools CP-SAT solver.
 from ortools.sat.python import cp_model
 
+def read_input(filename):
+    slice_num = 0 # l
+    job_num = 0 # 
+    cnt = 0
+    jobs = []
+    weights = []
+    print('line 13')
+    with open(filename) as input:
+        while True:
 
-def MinimalJobshopSat():
+            if cnt==0:
+                slice_num = int(input.readline())
+                cnt = cnt+1 
+            elif cnt==1:
+                job_num = int(input.readline())
+                cnt = cnt+1
+            else: 
+                print(cnt)
+                print('job_num', job_num)
+                
+                for i in range(job_num):
+                    job_tmp = []
+                    op_num = int(input.readline()) #number of operations for this job
+                    print('op_num', op_num)     
+                    weight = float(input.readline())
+                    print('weight', weight)
+                    weights.append(weight)
+                    for j in range(op_num):
+                        op = input.readline()
+                        print(op)
+                        op = op.split(' ')
+                        info_cnt = 0
+                        op_slice = 0
+                        op_dur = 0
+                        depend_num = 0
+                        op_depend = []
+                        for op_info in op:
+                            if info_cnt==0:
+                                op_slice = int(op_info)
+                                info_cnt = info_cnt+1
+                            
+                            elif info_cnt==1:
+                                op_dur = int(op_info)
+                                info_cnt = info_cnt+1
+                            
+                            elif info_cnt==2:
+                                depend_num = int(op_info)
+                                info_cnt = info_cnt+1
+                                
+                            else:
+                                for k in range(depend_num):
+                                    op_depend.append(int(op_info))
+                                
+                        print('op: ', (op_slice, op_dur, op_depend))
+                        job_tmp.append((op_slice, op_dur, op_depend))
+                        print('job_tmp', job_tmp)
+                    jobs.append(job_tmp)
+                break
+            
+    input.close()
+    #print(jobs)
+    return jobs, weights
+    
+
+
+
+def MinimalJobshopSat(jobs_data, weight):
     """Minimal jobshop problem."""
     # Create the model.
     model = cp_model.CpModel()
 
-    weight = [1.0, 1.0, 1.0]  # weight of each jobs
+    #weight = [1.0, 1.0, 1.0]  # weight of each jobs
+    """
     jobs_data = [  # task = (slice, processing_time, last dependency).
         [(1, 10, 0), (1, 5, 1), (1, 7, 1)],  # Job1
         [(2, 5, 0)],  # Job2
         [(1, 7, 0)]  # Job3
     ]
+    """
 
     machines_count = 3  # 有三個slice
 
     # Computes horizon dynamically as the sum of all durations.
-    horizon = sum(task[1] for job in jobs_data for task in job)
+    horizon = sum(task[1] for job in jobs_data for task in job) #sum of the duration of each op
+    
     # Named tuple to store information about created variables.
     task_type = collections.namedtuple(
         'task_type', 'start end interval')
@@ -47,9 +115,9 @@ def MinimalJobshopSat():
     # Precedences inside a job.
     for job_id, job in enumerate(jobs_data):
         for task_id, task in enumerate(job):
-            if task[2]:
+            for dep in task[2]:
                 model.Add(all_tasks[job_id, task_id].start >=
-                          all_tasks[job_id, task[2]-1].end)
+                          all_tasks[job_id, dep-1].end)
 
     # Makespan objective.
     obj_var = model.NewIntVar(0, horizon, 'makespan')
@@ -74,4 +142,9 @@ def MinimalJobshopSat():
                     all_tasks[job_id, task_id].start)+task[1]))
 
 
-MinimalJobshopSat()
+if __name__ == "__main__":
+    path = '../ada-final-public/'
+    name = path+'02.in'
+    
+    jobs_data, weight = read_input(name)
+    MinimalJobshopSat(jobs_data, weight)
